b0d9a2b7a3a672597ad409116f363cb42df3971f
Matilda578 <101889190@student.swin.edu.au>
2020-02-25 16:39:09 +1000

    prints names

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace FirstProg
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What's your Firstname?");
+            string firstname = Console.ReadLine();
+            Console.WriteLine("What is your Last Name?");
+            string lastname = Console.ReadLine();
+
+            string fullname = firstname + " " + lastname;
+
+            Console.WriteLine(fullname); 
+        }
+    }
+}
